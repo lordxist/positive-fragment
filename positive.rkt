4a6e2fd8b8a9f2c8bda7368117c37d1dfab94912
Julian Jabs <julian.jabs@web.de>
2018-02-15 15:32:33 +0100

    Implement rudimentary interpretation

--- positive.rkt
+++ positive.rkt
@@ -4,6 +4,7 @@
 
 (require (for-syntax racket/bool
                      racket/format
+                     racket/function
                      racket/list
                      racket/match
                      racket/string))
@@ -95,8 +96,9 @@
                                               #`(#,(datum->syntax s (string->symbol (string-append "i-" (symbol->string (syntax->datum #'sub)))))
                                                  type2 (bound-var ...) elem ...)])))
                                     arglist))])
-                            #`(list (list #,@(i-args (syntax->list #'(#,@args))))
-                                    (list #,@(i-args (syntax->list #'(#,@cargs))))))])))
+                            #`(list #,(make-prefab-struct (syntax->datum #'con))
+                               (list #,@(i-args (syntax->list #'(#,@args))))
+                               (list #,@(i-args (syntax->list #'(#,@cargs))))))])))
              #,(constructor-defs-for-type r prefix))))])]))
 
 (define-for-syntax (constructor-defs l prefix)
@@ -168,12 +170,43 @@
                   [binding-check-all
                    (map binding-check
                         (syntax->list #'((pattern-start pattern-type pattern-element ...) ...))
-                        (syntax->list #'((cmd-start cmd-element ...) ...)))])
+                        (syntax->list #'((cmd-start cmd-element ...) ...)))]
+                  [nvar (make-hash)]
+                  [match-vars (make-hash)]
+                  [structify
+                   (λ (p)
+                     (begin
+                       (unless (hash-has-key? nvar p)
+                            (begin
+                              (hash-set! nvar p -1)
+                              (hash-set! match-vars p empty)))
+                       (syntax-case p ()
+                         [(p-start p-type () ())
+                          (symbol=? 'p-var (syntax->datum #'p-start))
+                          (begin
+                            (hash-update! nvar p ((curry +) 1))
+                            (hash-update! match-vars p
+                                          ((curry cons) (datum->syntax #'p-start (string->symbol (string-append "bool-" (number->string (hash-ref nvar p)))))))
+                            (first (hash-ref match-vars p)))]
+                         [(p-start p-type (p-element1 ...) (p-element2 ...))
+                          (let ([structify-result1 (map structify (syntax->list #'(p-element1 ...)))]
+                                [structify-result2 (map structify (syntax->list #'(p-element2 ...)))])
+                            #`(list
+                               `#,(make-prefab-struct (string->symbol (substring (symbol->string (syntax->datum #'p-start)) 2)))
+                               (list #,@structify-result1)
+                               (list #,@structify-result2)))])))]
+                  [match-case
+                   #`(match case
+                       #,@(map (λ (p c) #`[#,(structify p)
+                                           (#,(binding-check p c)
+                                            #,@(reverse (hash-ref match-vars p)))])
+                               (syntax->list #'((pattern-start pattern-type pattern-element ...) ...))
+                               (syntax->list #'((cmd-start cmd-element ...) ...)))
+                       [_ '()])])
                ;#`(list (list (pattern-start pattern-type pattern-element ...) ...)
                ;        (list #,@binding-check-all
                ;              (fall-through-cmd-start fall-through-cmd-element ...)))
-               #``(#,@(map (λ (s) #`,#,s) binding-check-all))
-               )]))))
+               #`(λ (case) (list #,match-case (list (pattern-start pattern-type pattern-element ...) ...))))]))))
 
 (define-for-syntax command-def
   #`(...(define-syntax (cmd stx)
@@ -196,31 +229,32 @@
               (not (string=? (symbol->string (syntax->datum #'val-start)) "cmd"))
               (symbol=? (prefab-struct-key (syntax->datum #'cont-type))
                         (prefab-struct-key (syntax->datum #'val-type))))
-             #``(,(#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'cont-start)))))
-                   cont-type
-                   (bound-var ...)
-                   cont-element ...)
-                 ,(#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'val-start)))))
-                   val-type
-                   (bound-var ...)
-                   val-element ...))]
-            [(_ (bound-var ...) 'daemon name ((arg-start arg-type arg-element ...) ...))
+             #`((#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'cont-start)))))
+                 cont-type
+                 (bound-var ...)
+                 cont-element ...)
+                (#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'val-start)))))
+                 val-type
+                 (bound-var ...)
+                 val-element ...))]
+            [(_ (bound-var ...) daemon name ((arg-start arg-type arg-element ...) ...))
              (and
-              (string? (syntax->datum #'name))
+              (prefab-struct-key (syntax->datum #'name))
               (andmap
                (λ (s) (and
                        (not (string-prefix? (symbol->string (syntax->datum s)) "p-"))
                        (not (string=? (symbol->string (syntax->datum s)) "lambda"))
                        (not (string=? (symbol->string (syntax->datum s)) "cmd"))))
                (syntax->list #'(arg-start ...))))
-             #`(list
-                #,@(map (λ (s)
-                          (syntax-case s ()
-                            [(arg-start2 arg-type2 (bound-var2 ...) arg-element2 ...)
-                             #`(#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'arg-start2)))))
-                                arg-type2 (bound-var2 ...) arg-element2 ...)]))
-                        (syntax->list
-                         #'((arg-start arg-type (bound-var ...) arg-element ...) ...))))]))))
+             #``(#,(datum->syntax #'name (prefab-struct-key (syntax->datum #'name)))
+                 ,(list
+                   #,@(map (λ (s)
+                             (syntax-case s ()
+                               [(arg-start2 arg-type2 (bound-var2 ...) arg-element2 ...)
+                                #`(#,(datum->syntax stx (string->symbol (string-append "i-" (symbol->string (syntax->datum #'arg-start2)))))
+                                   arg-type2 (bound-var2 ...) arg-element2 ...)]))
+                           (syntax->list
+                            #'((arg-start arg-type (bound-var ...) arg-element ...) ...)))))]))))
 
 (define-for-syntax variable-def
   #`(define-syntax (var stx)
