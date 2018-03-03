bfdbdd77fd09677d8a409ab0131ee376634f8bb2
Julian Jabs <lordxist@users.noreply.github.com>
2018-03-03 14:33:54 +0100

    Generate synthetic abstraction types

--- profiles/data.rkt
+++ profiles/data.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (require (only-in "../positive.rkt" [data posdata])
-         (for-syntax racket/bool))
+         (for-syntax racket/bool
+                     racket/list
+                     racket/match))
 
 (provide #%module-begin data)
 
@@ -22,10 +24,28 @@
             [(_ elem ...)
              #`(#%module-begin #,@(map convert-calls (syntax->list #'(elem ...))))]))))
 
+(define-for-syntax (synthetic-abstraction-types l)
+  (map
+   (λ (ts)
+     (match ts
+       [(list t1 t2 t3)
+        (let
+            ([name
+              (datum->syntax #f (string->symbol (string-append "abstr-"
+                                                               (symbol->string (syntax->datum t1))
+                                                               "-"
+                                                               (symbol->string (syntax->datum t2))
+                                                               "-"
+                                                               (symbol->string (syntax->datum t3)))))])
+          #`(#,name ([#,name (#,t1 #,t2) (#,t3)])))]))     
+   (cartesian-product l l l)))
+
 (define-syntax (data stx)
   (syntax-case stx ()
     [(data (name ((con (type ...)) ...)) ...)
      #`(begin
          (provide (rename-out [module-begin #%module-begin]))
-         (posdata #s(preprocess) (name ((con (type ...) ()) ...)) ...)
+         (posdata #s(preprocess)
+                  (name ((con (type ...) ()) ...)) ...
+                  #,@(synthetic-abstraction-types (syntax->list #'(name ...))))
          #,module-begin-def)]))
