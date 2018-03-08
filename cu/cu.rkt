1366a7fa63fc05f50224e05dc8012f7f39dddd58
Julian Jabs <julian.jabs@web.de>
2018-03-08 16:53:47 +0100

    Make positive and negative phrases coexist.
    Without any shifting yet.

--- cu/cu.rkt
+++ cu/cu.rkt
@@ -12,8 +12,21 @@
      (data-helper #f #f #'(data (name ((con (type ...) (cnt-type ...)) ...)) ...))]))
 
 (define-syntax (codata stx)
-  (syntax-case stx ()
-    [(codata #s(recursive) (name ((con (type ...) (cnt-type ...)) ...)) ...)
-     (data-helper #t #f #'(data (name ((con (type ...) (cnt-type ...)) ...)) ...))]
-    [(codata (name ((con (type ...) (cnt-type ...)) ...)) ...)
-     (data-helper #f #f #'(data (name ((con (type ...) (cnt-type ...)) ...)) ...))]))
+  (let ([renaming #'(begin
+                      (require (rename-in 'cd
+                                          (lambda mu)
+                                          (cmd cmdn)
+                                          (var varn)
+                                          (p-var p-varn)))
+                      (provide mu cmdn varn p-varn))])
+    (syntax-case stx ()
+      [(codata #s(recursive) (name ((con (type ...) (cnt-type ...)) ...)) ...)
+       #`(begin
+           (module cd "../positive.rkt"
+             #,(data-helper #t #f #'(data (name ((con (type ...) (cnt-type ...)) ...)) ...)))
+         #,renaming)]
+      [(codata (name ((con (type ...) (cnt-type ...)) ...)) ...)
+       #`(begin
+           (module cd racket
+             #,(data-helper #f #f #'(data (name ((con (type ...) (cnt-type ...)) ...)) ...)))
+           #,renaming)])))
