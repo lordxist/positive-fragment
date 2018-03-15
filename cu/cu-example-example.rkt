74d36e921a6492ca26f9b23d74f9adccede639bd
Julian Jabs <julian.jabs@web.de>
2018-03-15 11:08:27 +0100

    Complete example pair

--- cu/cu-example-example.rkt
+++ cu/cu-example-example.rkt
@@ -44,3 +44,24 @@
                                   (succ #s(nat) ((succ #s(nat) ((zero #s(nat) () () ())) () ())) () ())))
                             (cmdn daemon #s(impossible) ()))))
         ()))
+
+; same as the above, but called with 0
+(cmdn
+ (mu #s(nat-fun) (((p-apply #s(nat-fun) ((p-shift #s(shift-nat) () () ((p-varn #s(nat) () () ())))) ((p-varn #s(shift-nat) () () ())) ())
+                   (cmdn
+                    (nvarn #s(shift-nat) 0 () ())
+                    (shift #s(shift-nat) () ()
+                           ((lambda #s(nat) (((p-zero #s(nat) () () ())
+                                              (cmd (nvar #s(nat) 0 () ()) (succ #s(nat) ((zero #s(nat) () () ())) () ())))
+                                             ((p-succ #s(nat) ((p-var #s(nat) () () ())) () ())
+                                              (cmd (nvar #s(nat) 0 () ()) (zero #s(nat) () () ())))
+                                             (cmd daemon #s(impossible) ())))))))
+                  (cmdn daemon #s(impossible) ())))
+ (apply #s(nat-fun)
+        ((shift #s(shift-nat) () () ((lambda #s(nat) (((p-var #s(nat) () () ()) (cmd daemon #s(print) ((var #s(nat) 0 () ()))))
+                                                      (cmd daemon #s(impossible) ()))))))
+        ((mu #s(shift-nat) (((p-shift #s(shift-nat) () () ((p-varn #s(nat) () () ())))
+                             (cmd (nvar #s(nat) 0 () ())
+                                  (zero #s(nat) () () ())))
+                            (cmdn daemon #s(impossible) ()))))
+        ()))
