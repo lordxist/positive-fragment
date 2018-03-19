944cfa6bf148895bbde567910a2fafd0defdbe36
Julian Jabs <julian.jabs@web.de>
2018-03-19 15:51:59 +0100

    [cu-data] Implement remaining desugarings

--- cu/profiles/data-sugar.rkt
+++ cu/profiles/data-sugar.rkt
@@ -1,15 +1,79 @@
 #lang racket
 
-(require (for-syntax racket/list
+(require (for-syntax racket/bool
+                     racket/list
                      racket/string))
 
-(provide primitive-apply primitive-pair)
+(provide primitive-print primitive-const primitive-fun primitive-apply primitive-pair)
 
-(define-syntax (primitive-apply stx)
+(define-syntax (primitive-print stx)
   (syntax-case stx ()
-    [(p (cont-head cont-type cont-elem ...) (arg-head arg-type arg-elem ...))
-     (let* ([in-type (string-join (rest (string-split (symbol->string (prefab-struct-key (syntax->datum #'arg-type))) "-")) "-")]
-            [out-type (last (string-split (symbol->string (prefab-struct-key (syntax->datum #'cont-type))) "-to-"))])
+    [(p type expr)
+     (datum->syntax
+      #'p
+      (syntax->datum
+       #`(cmdn
+          #,(primitive-apply-expand #'expr)
+          (#,(datum->syntax #f (string->symbol (string-append "shift" (string-replace (symbol->string (prefab-struct-key (syntax->datum #'type))) "-" ""))))
+           #,(make-prefab-struct (string->symbol (string-append "shift-" (symbol->string (prefab-struct-key (syntax->datum #'type))))))
+           () ()
+           ((lambda type
+              (((p-var type () () ()) (cmd daemon #s(print) ((var type 0 () ()))))
+               (cmd daemon #s(impossible) ()))))))))]))
+
+(define-for-syntax (primitive-const-expand stx)
+  (syntax-case stx ()
+    [(p (val-start val-type val-elem ...))
+     (datum->syntax
+      #'p
+      (syntax->datum
+       #`(mu #,(make-prefab-struct (string->symbol (string-append  "shift-" (symbol->string (prefab-struct-key (syntax->datum #'val-type))))))
+             (((#,(datum->syntax #f (string->symbol (string-append  "p-shift" (string-replace (symbol->string (prefab-struct-key (syntax->datum #'val-type))) "-" ""))))
+                #,(make-prefab-struct (string->symbol (string-append  "shift-" (symbol->string (prefab-struct-key (syntax->datum #'val-type))))))
+                () ()
+                ((p-varn val-type () () ())))
+               (cmd (nvar val-type 0 () ())
+                    (val-start val-type val-elem ...)))
+              (cmdn daemon #s(impossible) ())))))]))
+
+(define-syntax (primitive-const stx)
+  (primitive-const-expand stx))
+
+(define-for-syntax (primitive-fun-expand stx)
+  (syntax-case stx ()
+    [(p in-type out-type ((pattern case-expr) ...))
+     (let* ([fun-type
+             (string-append (symbol->string (prefab-struct-key (syntax->datum #'in-type))) "-to-"
+                            (symbol->string (prefab-struct-key (syntax->datum #'out-type))))]
+            [shift-out-type
+             (string-append "shift-" (symbol->string (prefab-struct-key (syntax->datum #'out-type))))])
+       (datum->syntax
+        #'p
+        (syntax->datum
+         #`(lambda #,(make-prefab-struct (string->symbol fun-type))
+             (#,@(map (λ (s)
+                        (syntax-case s ()
+                          [(pattern case-expr)
+                           #`((#,(datum->syntax #f (string->symbol (string-append "p-" (string-replace fun-type "-" ""))))
+                               #,(make-prefab-struct (string->symbol fun-type))
+                               (pattern)
+                               ((p-var out-type () () ()))
+                               ())
+                              (cmdn #,(primitive-const-expand #'case-expr)
+                                    (#,(datum->syntax #f (string->symbol (string-replace shift-out-type "-" "")))
+                                     #,(make-prefab-struct (string->symbol shift-out-type))
+                                     () () ((nvar out-type 0 () ())))))]))
+                      (syntax->list #'((pattern case-expr) ...)))
+              (cmd daemon #s(impossible) ()))))))]))
+
+(define-syntax (primitive-fun stx)
+  (primitive-fun-expand stx))
+
+(define-for-syntax (primitive-apply-expand stx)
+  (syntax-case stx ()
+    [(p (fun-head fun-in-type fun-out-type fun-elem ...) (arg-head arg-elem ...))
+     (let ([in-type (symbol->string (prefab-struct-key (syntax->datum #'fun-in-type)))]
+           [out-type (symbol->string (prefab-struct-key (syntax->datum #'fun-out-type)))])
        (datum->syntax
         #'p
         (syntax->datum
@@ -21,14 +85,15 @@
                #,(make-prefab-struct (string->symbol (string-append "shift-" out-type)))
                () ()
                ((p-varn #,(make-prefab-struct (string->symbol out-type)) () () ())))
-
-              (cmdn (arg-head arg-type arg-elem ...)
+              (cmdn #,(if (symbol=? (syntax->datum #'arg-head) 'primitive-const)
+                          (primitive-const-expand #'(arg-head arg-elem ...))
+                          #'(arg-head arg-elem ...))
                     (#,(datum->syntax #f (string->symbol (string-replace (string-append "shift" in-type) "-" "")))
                      #,(make-prefab-struct (string->symbol (string-append "shift-" in-type)))
                      () ()
                      ((lambda #,(make-prefab-struct (string->symbol in-type))
                         (((p-var #,(make-prefab-struct (string->symbol in-type)) () () ())
-                          (cmd (cont-head cont-type cont-elem ...)
+                          (cmd #,(primitive-fun-expand #'(fun-head fun-in-type fun-out-type fun-elem ...))
                                (#,(datum->syntax #f (string->symbol (string-replace (string-append in-type "to" out-type) "-" "")))
                                 #,(make-prefab-struct (string->symbol (string-append in-type "-to-" out-type)))
                                 ((var #,(make-prefab-struct (string->symbol in-type)) 0 () ()))
@@ -36,6 +101,9 @@
                                 ())))
                          (cmd daemon #s(impossible) ())))))))
              (cmdn daemon #s(impossible) ()))))))]))
+
+(define-syntax (primitive-apply stx)
+  (primitive-apply-expand stx))
 
 ; from "On the unity of duality", p.25
 (define-syntax (primitive-pair stx)
